6f7b646519992e3507766b9503372306d7caf1c2
Julleon <julleonchik@gmail.com>
2024-02-09 21:07:45 +0900

    Initial commit of a simple REST service using Spring Boot

---
+++ src/main/java/pro/julleon/showcasespringbootrest/ShowcaseSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package pro.julleon.showcasespringbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseSpringBootRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowcaseSpringBootRestApplication.class, args);
+    }
+
+}
